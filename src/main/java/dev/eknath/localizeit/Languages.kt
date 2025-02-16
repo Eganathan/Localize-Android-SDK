5f3a40425cdcd8b271f863352f9364ab0a585256
Eganathan R <ammasamrith@gmail.com>
2025-02-16 08:21:36 +0530

    chore: initial commit

---
+++ src/main/java/dev/eknath/localizeit/Languages.kt
@@ -0,0 +1,112 @@
+package dev.eknath.localizeit
+
+import android.app.LocaleManager
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Build
+import android.os.LocaleList
+import androidx.appcompat.app.AppCompatDelegate
+import java.util.Locale
+
+
+sealed class Language {
+
+    //common properties
+    abstract val code: String
+    abstract val isRtl: Boolean
+
+    companion object {
+
+        //Make sure you add all the languages here
+        val allowedLocales = listOf(English, Tamil, Hindi, Malayalam, French)
+
+        fun setLocale(context: Context, localeCode: String) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                context.getSystemService(LocaleManager::class.java).applicationLocales =
+                    LocaleList.forLanguageTags(localeCode)
+            } else {
+                saveToLocalSharedAndUpdateResources(context = context, localeTag = localeCode)
+            }
+        }
+
+        internal fun getCurrentLanguage(context: Context): Language {
+            return this.allowedLocales.find { it.code == getCurrentLocale(context) } ?: English
+        }
+
+        private fun getCurrentLocale(context: Context): String {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                context.getSystemService(LocaleManager::class.java).applicationLocales.toLanguageTags()
+            } else {
+                AppCompatDelegate.getApplicationLocales().toLanguageTags()
+            }
+        }
+
+
+        // REQUIRED FOR API LEVEL 32 AND LOWER AND/OR FOR WITHOUT ACTIVITY RE-CREATION
+        private const val sharedPrefSelectedLocaleKey = "LocalePreference"
+        private fun getLocaleSharedPreference(context: Context): SharedPreferences? {
+            return context.applicationContext?.getSharedPreferences(
+                sharedPrefSelectedLocaleKey,
+                Context.MODE_PRIVATE
+            )
+        }
+
+        private fun setLocaleForDevicesLowerThanTiramisu(localeTag: String, context: Context) {
+            val locale = Locale(localeTag)
+            Locale.setDefault(locale)
+            val resources = context.resources
+            val configuration = resources.configuration
+            configuration.setLocale(locale)
+            resources.updateConfiguration(configuration, resources.displayMetrics)
+        }
+
+        private fun saveToLocalSharedAndUpdateResources(context: Context, localeTag: String) {
+            val sharedPref = getLocaleSharedPreference(context) ?: return
+
+            with(sharedPref.edit()) {
+                putString(sharedPrefSelectedLocaleKey, localeTag)
+                apply()
+            }
+            setLocaleForDevicesLowerThanTiramisu(localeTag, context)
+        }
+
+        //This needs to be called OnStart() or OnCreation() of the Activity for Android API level 32 and below
+        fun configureLocaleOnStartForDevicesLowerThanTiramisu(context: Context) {
+            setLocaleForDevicesLowerThanTiramisu(
+                context = context,
+                localeTag = getLocaleSharedPreference(context)?.getString(sharedPrefSelectedLocaleKey, English.code) ?: English.code
+            )
+        }
+    }
+
+    object English : Language() {
+        override val code: String = "en"
+        override val isRtl: Boolean = false
+    }
+
+    object Tamil : Language() {
+        override val code: String = "ta"
+        override val isRtl: Boolean = false
+    }
+
+    object Malayalam : Language() {
+        override val code: String = "ml"
+        override val isRtl: Boolean = false
+    }
+
+    object Hindi : Language() {
+        override val code: String = "hi"
+        override val isRtl: Boolean = false
+    }
+
+    object French : Language() {
+        override val code: String = "fr"
+        override val isRtl: Boolean = false
+    }
+
+    object Arabic : Language() {
+        override val code: String = "ar"
+        override val isRtl: Boolean = false
+    }
+
+}
---
+++ src/main/java/dev/eknath/localizeit/LocalizeIt.kt
@@ -0,0 +1,49 @@
+package dev.eknath.localizeit
+
+import android.app.Application
+import android.content.Context
+import android.os.Build
+import android.os.LocaleList
+import android.util.Log
+
+object LocalizeIt {
+    lateinit var supportedLocales: List<String>
+
+    fun init(context: Application) {
+        supportedLocales = context.resources.assets.locales.toString().replaceFirst("[", "").replaceAfterLast("]", "").split(",")
+
+        ensureAllTranslationExists(context)
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU)
+            setLocale(context = context, "tx")
+    }
+
+    fun setLocale(context: Context, languageCode: String) {
+        if (!supportedLocales.contains(languageCode))
+            throw IllegalArgumentException("Invalid Language Code : $languageCode, defined supported languages are $supportedLocales")
+
+        Language.setLocale(context, languageCode)
+    }
+
+
+    fun ensureAllTranslationExists(context: Context){
+        getAllStringResourceNames(context).forEach {
+            Log.e("Test","T:$it")
+        }
+    }
+
+    private fun getAllStringResourceNames(context: Context): List<String> {
+        val resources = context.resources
+        val packageName = context.packageName
+        val rClass = Class.forName("$packageName.R\$string")
+
+        return rClass.fields.mapNotNull { field ->
+            try {
+                val resId = field.getInt(null) // Get resource ID
+                resources.getString(resId) // Check if it exists
+                field.name // Return the resource name
+            } catch (e: Exception) {
+               throw IllegalArgumentException("No translation found for $field")
+            }
+        }
+    }
+}
